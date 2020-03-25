b682f52de00f1d86c50aeeffc2b7a8034dc0a83b
sirawat <sirawat@netapp.com>
2020-03-25 10:57:35 +0700

    edit command

--- command.sh
+++ command.sh
@@ -2,3 +2,6 @@ lun create -vserver svm_CVO01 -volume vol3 -lun lun0 -size 50GB -ostype linux -s
 
 lun map -vserver svm_CVO01 -volume vol3 -lun lun0 -igroup trident2
 
+wget https://github.com/NetApp/trident/releases/download/v20.01.0/trident-installer-20.01.0.tar.gz
+tar -xf trident-installer-20.01.0.tar.gz
+cd trident-installer
