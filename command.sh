fac45967caf4fc0217a4cad77f0c193db604e9ae
fpunthira <fsirapol@gmail.com>
2020-05-11 16:00:39 +0700

    Update command.sh
    edit comand.sh

--- command.sh
+++ command.sh
@@ -5,3 +5,5 @@ lun map -vserver svm_CVO01 -volume vol3 -lun lun0 -igroup trident2
 wget https://github.com/NetApp/trident/releases/download/v20.01.0/trident-installer-20.01.0.tar.gz
 tar -xf trident-installer-20.01.0.tar.gz
 cd trident-installer
+
+ls -la
